fdc57621373725b63f57e4c15012d523609cb13e
syedlutfi27 <syedlutfi273@gmail.com>
2025-01-05 21:39:58 +0530

    Updated portfolio

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ function Header() {
         <a className="font-bold text-black" href="#">Syed Lutfi</a>
         <nav className="hidden md:block">
             <ul className="flex text-white ">
-                <li><a href="/">Home</a></li>
+                <li><a href="#hero">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#projects">Projects</a></li>
                 <li><a href="#resume">Resume</a></li>
@@ -20,7 +20,7 @@ function Header() {
         </nav>
         {toggleMenu && <nav className="block md:hidden ">
             <ul onClick={() => setToggleMenu(!toggleMenu)} className="flex flex-col text-white  mobile-nav">
-                <li><a href="#">Home</a></li>
+                <li><a href="#hero">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#projects">Projects</a></li>
                 <li><a href="#contact">Contact</a></li>
