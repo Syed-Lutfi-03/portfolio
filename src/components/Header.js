429ed1a354558f712a6574400acf1aea934ece22
syedlutfi27 <syedlutfi273@gmail.com>
2025-01-05 21:31:39 +0530

    Updated portfolio

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,10 +12,10 @@ function Header() {
         <nav className="hidden md:block">
             <ul className="flex text-white ">
                 <li><a href="/">Home</a></li>
-                <li><a href="/#about">About</a></li>
-                <li><a href="/#projects">Projects</a></li>
-                <li><a href="/#resume">Resume</a></li>
-                <li><a href="/#contact">Contact</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#resume">Resume</a></li>
+                <li><a href="#contact">Contact</a></li>
             </ul>
         </nav>
         {toggleMenu && <nav className="block md:hidden ">
